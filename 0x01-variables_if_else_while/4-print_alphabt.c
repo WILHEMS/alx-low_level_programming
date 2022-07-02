ecc601980baf5d5862fc6f7895494750c143033c
WILHEMS <derekwilhems@gmail.com>
2022-07-01 19:41:16 -0700

    Low level Programming:Variables,if...else,while

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * * main - Print the alphabet in lowercase letters, except for e and q
- * *
- * * Return: Always 0 (Success)
- * */
+* main - Print the alphabet in lowercase letters, except for e and q
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
 	char letter;
