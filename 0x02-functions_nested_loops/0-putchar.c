e2353d6a9247cd972785cb498d6e770f3aecb043
WILHEMS <derekwilhems@gmail.com>
2022-07-05 19:34:42 -0700

    Low level

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,25 @@
+#include "holberton.h"
+
+
+
+/**
+ *
+ * * main - prints Holberton, followed by a new line
+ *
+ * * Return: Always 0 (Success)
+ *
+ * */
+
+int main(void)
+
+{
+
+	_putchar('H');
+
+
+
+
+	return (0);
+
+}
+
