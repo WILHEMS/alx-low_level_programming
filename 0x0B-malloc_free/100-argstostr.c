203fbc80e7867fbbcf17bc5bc31330f71b3ef8ae
WILHEMS <derekwilhems@gmail.com>
2022-07-26 18:25:03 -0700

    Malloc free

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -24,7 +24,9 @@ char *argstostr(int ac, char **av)
 	if (ac == 0 || av == NULL)
 		return (NULL);
 	size = 0;
-	/* count the number of chars in each string */
+	/* 
+	 * count the number of chars in each string
+	 */
 	for (i = 0; i < ac; i++)
 	{
 		for (j = 0; av[i][j] != '\0'; j++)
@@ -32,7 +34,8 @@ char *argstostr(int ac, char **av)
 		size++;
 	}
 	size++;
-	/* allocate memory for total number of chars and
+	/*
+	 * allocate memory for total number of chars and
 	 *
 	 * new line for each word
 	 *
