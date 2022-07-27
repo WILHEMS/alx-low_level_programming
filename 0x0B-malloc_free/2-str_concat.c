2844fcbc7f33c91d7541f53935265e2383e355a5
WILHEMS <derekwilhems@gmail.com>
2022-07-26 18:14:46 -0700

    Malloc free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -31,35 +31,19 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 		j++;
 	strDup = malloc(sizeof(char) * (i + j + 1));
-
 	if (strDup == NULL)
-
 		return (NULL);
-
 	i = j = 0;
-
 	while (s1[i] != '\0')
-
 	{
-
 		strDup[i] = s1[i];
-
 		i++;
-
 	}
-
 	while (s2[j] != '\0')
-
 	{
-
 		strDup[i] = s2[j];
-
 		i++, j++;
-
 	}
-
 	strDup[i] = '\0';
-
 	return (strDup);
-
 }
